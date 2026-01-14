4974ffd92340fe28afac64fb6097b033b15041bd
Portono <evan.miniscloux@gmail.com>
2026-01-14 15:40:52 +0100

    Update Main_game.py

--- In-game/Main_game.py
+++ In-game/Main_game.py
@@ -1,216 +1,172 @@
 import pygame
+import random
+import math
+from Menu import *
 
 pygame.init()
 
-#Dimensions de la fenetre
-#acquisition de la resolution de l'ecran
-monitor_info=pygame.display.Info()
-monitor_width=monitor_info.current_w   #=valeur de test (temporaire)
-monitor_height=monitor_info.current_h  #=valeur de test (temporaire)
-width=monitor_width
-height=monitor_height
+ennemy_spawn_delay=2000  ##Délai entre chaque spawn d'ennemi en millisecondes TEMPORAIRE
 
-#Variables de gestion du mode plein ecran
-fullscreen_change=False  #True si bouton Fulscreen appuye Temporaire, doit etre False au debut mais peut varier selon les parametres sauvegardes
-fullscreen=True    #Etat de plein ecran, doit etre True au debut mais peut varier selon les parametres sauvegardes
-resolution_change=True  #True si resolution changee doit etre True au debut pour tout initialiser mais peut varier selon les parametres sauvegardes
-user_width_input="Largeur"  #Variable pour stocker l'input utilisateur pour la largeur
-width_input_toggle=False  #Variable pour activer/desactiver l'input de la largeur
-user_height_input="Hauteur"  #Variable pour stocker l'input utilisateur pour la hauteur
-height_input_toggle=False  #Variable pour activer/desactiver l'input de la hauteur
+# Configuration de la fenêtre
+screen = pygame.display.set_mode((width, height)) 
+#Classes
+class projectiles_general:
+    """Classe principale des projectiles"""
+    def __init__(self,x,y,vitesse,cible_initiale,homing=False,sprite_path=None,couleur=(0,255,0)):  ##AJOUTER PLUS TARD SPRITE AVEC CHEMIN D'ACCES ET COULEUR SERT SEULEMENT SI PAS DE SPRITE
+        self.x=x
+        self.y=y
+        self.vitesse=vitesse
+        self.cible=cible_initiale
+        self.homing=homing
+        self.couleur=couleur
 
-#predefinission des couleurs utiles
-white=(255,255,255)
-black=(0,0,0)
-red=(255,0,0)
-green=(0,255,0)
-blue=(0,0,255)
-yellow=(255,255,0)
-orange=(255,165,0)
-gray=(128,128,128)
+        self.image = None
+        if sprite_path:
+            self.image = pygame.image.load(sprite_path).convert_alpha()
+            self.rect = pygame.transform.scale(self.image, (20, 20))
+            self.rect = self.image.get_rect(center=(self.x, self.y))
+        else:
+            self.rect = pygame.Rect(self.x, self.y, 10, 10)
+            self.rect.center = (self.x, self.y)
+        #Si cible meurt entre temps, le projectile continue tout droit
+        self.calculer_direction(cible_initiale.x, cible_initiale.y)
 
-#Predeffinission de la police et de la couleur des boutons
-menu_font=pygame.font.Font("font.ttf", int(height*0.05)) ##Definition de la police et de la taille du texte des boutons
-button_color=black
-hover_color=gray
-text_color=orange
-height_button_text="Hauteur"
-width_button_text="Largeur"
-#definition des dimensions des boutons
-button_width=int(width*0.25)
-button_height=int(height*0.1)
+    def calculer_direction(self, cible_x, cible_y):
+        direction_x = cible_x - self.x
+        direction_y = cible_y - self.y
+        distance= math.hypot(direction_x, direction_y)
+        if distance != 0:
+            self.dir_x = direction_x / distance
+            self.dir_y = direction_y / distance
 
-#Definition des boutons
-play_button_rect=pygame.Rect(0,0,button_width,button_height)
-settings_button_rect=pygame.Rect(0,0,button_width,button_height)
-quit_button_rect=pygame.Rect(0,0,button_width,button_height)
-fullscreen_button_rect=pygame.Rect(0, 0, button_width, button_height)
-goback_button_rect=pygame.Rect(0, 0, button_width, button_height)
-input_width_rect=pygame.Rect(0, 0, button_width, button_height)
-input_height_rect=pygame.Rect(0, 0, button_width, button_height)
-#Definition de texte des boutons
-height_button_text="Hauteur"
-width_button_text="Largeur"
-#Creation de la fenetre
-screen=pygame.display.set_mode((width,height), pygame.FULLSCREEN)   ##Definition de la fenetre avec la resolution
-pygame.display.set_caption("Champ de Mars") ##Titre de la fenetre
-
-#Ajout du logo + redissionnement adaptatif a la resolution choisie
-logo_import=pygame.image.load("logo_champ_de_mars.png")
-logo_import_width=logo_import.get_width()
-logo_import_height=logo_import.get_height()
-logo=pygame.transform.smoothscale(logo_import,(width,int(logo_import_height/logo_import_width*width)))
-screen.fill(white)
-
-#on définit les variables pour les changement de scène du menu
-menu_main="main"
-menu_settings="settings"
-current_menu=menu_main
-play=False  ##Variable pour lancer le jeu
-
-
-def refresh_ui():
-    """
-    Cette fonction sert a rafraichir l'interface utilisateur en repositionnant les boutons et le logo en recalculant leurs positions et leurs tailles
-    """
-    global play_button_rect, settings_button_rect, quit_button_rect, logo, menu_font, fullscreen_button_rect, goback_button_rect, input_width_rect,input_height_rect
-    monitor_info=pygame.display.Info()
-    monitor_width=monitor_info.current_w
-    monitor_height=monitor_info.current_h
-    if fullscreen==True:
-        screen=pygame.display.set_mode((width,height), pygame.FULLSCREEN)
-    elif fullscreen==False:
-        screen=pygame.display.set_mode((width,height))
-    #Repositionnement du logo
-    logo=pygame.transform.smoothscale(logo_import,(width,int(logo_import_height/logo_import_width*width)))
-    #Repositionnement des boutons
-    button_width=int(width*0.25)
-    button_height=int(height*0.1)
-    ##Bouton Play
-    play_button_rect=pygame.Rect(0,0,button_width,button_height)
-    play_button_rect.center=(width//2, height//3.8)
-    ##Bouton Settings
-    settings_button_rect=pygame.Rect(0,0,button_width,button_height)
-    settings_button_rect.center=(width//2, height*2//3.8)
-    ##Bouton Quit
-    quit_button_rect=pygame.Rect(0,0,button_width,button_height)
-    quit_button_rect.center=(width//2, height*3//3.8)
-    ##Bouton Settings-Fullscreen
-    fullscreen_button_rect=pygame.Rect(0, 0, button_width/1.25, button_height/1.25)
-    fullscreen_button_rect.center = (width // 2, height*2 // 3.8)
-    ##Bouton Settings-Go Back
-    goback_button_rect=pygame.Rect(0, 0, button_width, button_height)
-    goback_button_rect.center = (width//2, height*3//3.8)
-    ##Bouton Settings-Input Width
-    input_width_rect=pygame.Rect(0, 0, button_width/1.5, button_height/1.5)
-    input_width_rect.center = (width // 1.5, height // 3)   
-    ##Bouton Settings-Input Height
-    input_height_rect=pygame.Rect(0, 0, button_width/1.5, button_height/1.5)
-    input_height_rect.center = (width // 3, height // 3)
-    ##Taille de la police
-    menu_font=pygame.font.Font("font.ttf", int(height*0.05))
-
-while play!=True:   ##Boucle principale du menu
-    #Recuperation de la position de la souris
-    mouse_pos=pygame.mouse.get_pos()
-    #Raffraichissement du logo sur l'ecran
-    screen.fill(white)  ##Fond blanc
-    screen.blit(logo,(0,0))     ##Affichage du logo en haut de l'ecran
-
-    #Quitter le jeu
-    for event in pygame.event.get():    ##Recuperation des evenements
-        if event.type == pygame.QUIT:   ##Si on clique sur la croix
-            pygame.quit()       ##Quitte pygame
-            exit()      ##Quitte le programme
-        if event.type == pygame.MOUSEBUTTONDOWN:   ##Si un bouton de la souris est appuye 
-            #   Main Menu   #
-            if current_menu==menu_main: ##Si on est dans le menu principal
-                width_input_toggle,height_input_toggle=False,False
-                if play_button_rect.collidepoint(mouse_pos):   ##Si le bouton Play est appuye
-                    play=True
-                if settings_button_rect.collidepoint(mouse_pos):   ##Si le bouton Settings est appuye
-                    current_menu=menu_settings
-                if quit_button_rect.collidepoint(mouse_pos):   ##Si le bouton Quit est appuye
-                    pygame.quit()       ##Quitte pygame
-                    exit()      ##Quitte le programme
-            #   Settings Menu   #
-            elif current_menu==menu_settings: ##Si on est dans le menu des parametres
-                
-                if fullscreen_button_rect.collidepoint(mouse_pos):   ##Si le bouton Fullscreen est appuye
-                    fullscreen_change=True
-                    fullscreen=not fullscreen
-                if goback_button_rect.collidepoint(mouse_pos):   ##Si le bouton Go Back est appuye
-                    current_menu=menu_main ##on retourne au menu principal
-                if input_width_rect.collidepoint(mouse_pos):   ##Si le bouton de la largeur est appuye
-                    width_input_toggle=True
-                    width_button_text="Largeur"
-                    user_width_input=""
-                if input_height_rect.collidepoint(mouse_pos):   ##Si le bouton de la hauteur est appuye
-                    height_input_toggle=True
-                    height_button_text="Hauteur"
-                    user_height_input=""
-
-        ##Changement de la resolution via l'input utilisateur
-        if event.type==pygame.KEYDOWN and current_menu==menu_settings and width_input_toggle==True:   ##Si une touche est appuye dans le menu des parametres
-            if event.key==pygame.K_RETURN or event.key==pygame.K_KP_ENTER:   ##Si la touche entree est appuyee
-                try:
-                    width=int(user_width_input)   ##Conversion de l'input utilisateur en entier
-                    resolution_Change=True
-                    width_input_toggle=False
-                    user_width_input=width_button_text  ##Reset de l'input utilisateur
-                except ValueError:
-                    print("Invalid width input")   ##Message d'erreur pour input invalide
-            elif event.key==pygame.K_BACKSPACE:   ##Si la touche retour est appuyee
-                user_width_input=user_width_input[:-1]   ##Supprime le dernier caractere de l'input utilisateur
+    def update(self,liste_ennemis):
+        if self.homing:
+            #Recalcule la direction vers la cible
+            if self.cible in liste_ennemis:
+                self.calculer_direction(self.cible.x, self.cible.y)
             else:
-                user_width_input+=event.unicode   ##Ajoute le caractere appuye a l'input utilisateur
+                if liste_ennemis:
+                    self.cible = min(liste_ennemis, key=lambda ennemi: math.hypot(ennemi.x - self.x, ennemi.y - self.y))
+        #Mouvement du projectile
+        self.x += self.dir_x * self.vitesse
+        self.y += self.dir_y * self.vitesse
+        self.rect.center = (self.x, self.y)
 
-        if event.type == pygame.KEYDOWN and current_menu==menu_settings and height_input_toggle==True:   ##Si une touche est appuye dans le menu des parametres
-            if event.key==pygame.K_RETURN or event.key==pygame.K_KP_ENTER:   ##Si la touche entree est appuyee
-                try:
-                    height=int(user_height_input)   ##Conversion de l'input utilisateur en entier
-                    resolution_change=True
-                    height_input_toggle=False
-                    user_height_input=height_button_text  ##Reset de l'input utilisateur
-                except ValueError:
-                    print("Invalid height input")   ##Message d'erreur pour input invalide
-            elif event.key==pygame.K_BACKSPACE:   ##Si la touche retour est appuyee
-                user_height_input=user_height_input[:-1]   ##Supprime le dernier caractere de l'input utilisateur
-            else:
-                user_height_input+=event.unicode   ##Ajoute le caractere appuye a l'input utilisateur
+    def dessiner(self,screen,offset_x,offset_y):
+        pos_ecran=(self.rect.x-offset_x,self.rect.y - offset_y)
+        if self.image:
+            screen.blit(self.image, pos_ecran)
+        else:
+            pygame.draw.rect(screen,self.couleur,(pos_ecran[0],pos_ecran[1],10,10))
 
-    #dessine les boutons et le texte
-    if current_menu==menu_main: ##les boutons dans le menu principal
-        user_width_input=width_button_text
-        user_height_input=height_button_text
-        for rect,texte in [(play_button_rect,"Play"),(settings_button_rect,"Parametres"),(quit_button_rect,"Quitter")]:
-            if rect.collidepoint(mouse_pos):    ##Si la souris est au dessus du bouton
-                button_color=hover_color    ##Change la couleur du bouton
-            else:
-                button_color=black
-            pygame.draw.rect(screen,button_color, rect)   ##Dessin du bouton
-            texte_surface=menu_font.render(texte,True,orange)    ##Creation du texte
-            texte_rect=texte_surface.get_rect(center=rect.center)   ##Centrage du texte
-            screen.blit(texte_surface, texte_rect)  ##Affichage du texte
-    if current_menu==menu_settings: #les boutons dans le menu des parametres
-        #Texte de la resolution actuelle
-        resolution_texte=menu_font.render(f"Resolution: {width}x{height}",True,black)  ##Creation du texte de la resolution
-        resolution_texte_rect=resolution_texte.get_rect(center=(width//2, height//5))   ##Centrage du texte de la resolution
-        screen.blit(resolution_texte, resolution_texte_rect)  ##Affichage du texte
-        for rect,texte in [(fullscreen_button_rect,"Plein ecran"),(goback_button_rect,"Retour"),(input_width_rect,user_width_input),(input_height_rect,user_height_input)]:
-            if rect.collidepoint(mouse_pos):    ##Si la souris est au dessus du bouton
-                button_color=hover_color    ##Change la couleur du bouton
-            else:
-                button_color=black
-            pygame.draw.rect(screen,button_color, rect)   ##Dessin du bouton
-            texte_surface=menu_font.render(texte,True,orange)    ##Creation du texte
-            texte_rect=texte_surface.get_rect(center=rect.center)   ##Centrage du texte
-            screen.blit(texte_surface, texte_rect)  ##Affichage du texte
+class projectile_laser(projectiles_general):
+    """Classe des projectiles laser"""
+    def __init__(self,x,y,vitesse,cible_initiale,homing=False):
+        super().__init__(x,y,vitesse,cible_initiale,homing=homing, sprite_path=None, couleur=(255,0,0))  ##Appelle le constructeur de la classe parente avec une couleur rouge
+        
+class weapon_main:
+    """Classe principale des armes"""
+    def __init__(self,delai,classe_projectile,homing=False,range_max=1/2*height):
+        self.delai=delai  ##Temps entre chaque tir en millisecondes
+        self.dernier_tir=0  ##Temps du dernier tir
+        self.classe=classe_projectile  ##Classe du projectile tiré
+        self.homing=homing  ##Indique si les projectiles sont homing ou non
+        self.range=range_max  ##Portée maximale de l'arme
+    def tirer(self, temps_actuel):
+        temps_actuel = pygame.time.get_ticks()
+        if temps_actuel - self.dernier_tir >= self.delai:
+            self.dernier_tir = temps_actuel
+            return  True
+        return False
+    
 
-    #Toggle du fullscreen
-    if fullscreen_change==True or resolution_change==True:
-        fullscreen_change=False
-        resolution_change=False
-        refresh_ui()
+#Variables de jeu
+player_x, player_y = 0, 0 # Position réelle du joueur dans le monde
+vitesse_joueur = 10
+couleur_joueur = (255, 0, 0)
+types_ennemis = [ennemi_simple, ennemi_rapide,ennemi_tireur]  ##Liste des types d'ennemis
+liste_ennemis = []  ##Liste pour stocker les ennemis
+derniers_spawn = {classe: 0 for classe in types_ennemis}  ##Dictionnaire pour stocker le dernier spawn de chaque type d'ennemi
+clock = pygame.time.Clock()
+liste_projectiles = []  ##Liste pour stocker les projectiles
+laser=weapon_main(500, projectile_laser,homing=False,range_max=1/2*height)  ##Crée une arme laser avec un délai de 500ms entre chaque tir et des projectiles homing
+type_armes=[laser]   ##Liste des types d'armes
+liste_projectiles_ennemis=[]  ##Liste pour stocker les projectiles des ennemis
+
+
+while play == True:
+    clock.tick(60)
+    temps_actuel = pygame.time.get_ticks()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    # Dessiner le fond
+    screen.fill((255, 255, 255)) # Fond blanc
+
+    #Modifie les coordonnees reelles du joueur
+    touches = pygame.key.get_pressed()
+    if touches[pygame.K_d]:
+        player_x += vitesse_joueur
+    if touches[pygame.K_q] or touches[pygame.K_a]:
+        player_x -= vitesse_joueur
+    if touches[pygame.K_s]:
+        player_y += vitesse_joueur
+    if touches[pygame.K_z] or touches[pygame.K_w]:
+        player_y -= vitesse_joueur
+
+    #Maintient le joueur au centre de l'ecran en calculant le decalage
+    offset_x = player_x - (width // 2)
+    offset_y = player_y - (height // 2)
+
+    # Dessiner une grille pour voir le mouvement de la caméra
+    for i in range(-5000, 5000, 100):
+        pygame.draw.line(screen, (240, 240, 240), (i - offset_x, -5000 - offset_y), (i - offset_x, 5000 - offset_y))
+        pygame.draw.line(screen, (240, 240, 240), (-5000 - offset_x, i - offset_y), (5000 - offset_x, i - offset_y))
+
+    # Gérer le spawn des ennemis
+    for classe in types_ennemis:
+        if temps_actuel - derniers_spawn[classe] >= classe.spawn_delay:
+            spawn_x, spawn_y = classe.calculer_pos_spawn(player_x, player_y, width, height)
+            nouvel_ennemi = classe(spawn_x, spawn_y)
+            liste_ennemis.append(nouvel_ennemi)
+            derniers_spawn[classe] = temps_actuel
+    # Mettre à jour et dessiner les ennemis
+    for ennemi in liste_ennemis:
+        ennemi.update(player_x, player_y)
+        ennemi.dessiner(screen, offset_x, offset_y)
+    # Dessiner le joueur au centre de l'écran (Vraies coordonnees)
+    player_real_rect = pygame.Rect(0,0, 50, 50)
+    player_real_rect.center = (player_x, player_y)
+    #Dessine le joueur
+    player_screen_rect = pygame.Rect(width // 2, height // 2, 50, 50)
+    player_screen_rect.center = (width // 2, height // 2)
+    pygame.draw.rect(screen, couleur_joueur, player_screen_rect)
+    #Gerer le tir du joueur
+    if liste_ennemis:
+        cible_proche= min(liste_ennemis, key=lambda ennemi: math.hypot(ennemi.x - player_x, ennemi.y - player_y))
+        for armes in type_armes:
+            if armes.tirer(temps_actuel) and math.hypot(cible_proche.x - player_x, cible_proche.y - player_y)<=armes.range:
+                nouveau_projectile = armes.classe(player_x, player_y, 15, cible_proche, homing=armes.homing)
+                liste_projectiles.append(nouveau_projectile)
+
+    #Dessiner les projectiles du joueur
+    for proj in liste_projectiles[:]:
+        proj.update(liste_ennemis)
+        proj.dessiner(screen, offset_x, offset_y)
+
+    #Gerer les tirs des ennemis
+    for proj in liste_projectiles_ennemis[:]:
+        proj.update([])
+        proj.dessiner(screen, offset_x, offset_y)
+
+        if proj.rect.colliderect(player_real_rect):
+            liste_projectiles_ennemis.remove(proj)
+        elif math.hypot(proj.x - player_x, proj.y - player_y) > height:
+            liste_projectiles_ennemis.remove(proj)
+            #Gerer les degats au joueur ici
     pygame.display.flip()
 
+pygame.quit()
