751adc3f3158b5ab037edeecd6dabdc53bd4b7d1
Portono <evan.miniscloux@gmail.com>
2026-01-18 11:34:12 +0100

    Add main function to initialize pygame and run menu

--- Main.py
+++ Main.py
@@ -1,3 +1,18 @@
+import pygame
 from Main_game import*
 from Sounddesign import*
-from Menu import*
+from Menu import *
+
+def main():
+    pygame.init()
+    running=True
+    while running:
+        settings=afficher_menu()
+        if settings["play"]:
+            lancer_jeu(settings)
+        else:
+            running=False
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
