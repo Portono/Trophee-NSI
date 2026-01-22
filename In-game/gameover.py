8dd644fa275a919018d264048c9aaadea0502eea
Enma-EB <berthelinevan@gmail.com>
2026-01-22 14:41:27 +0100

    Integrate sound effect on game over
    Added sound effect for game over screen.

--- In-game/gameover.py
+++ In-game/gameover.py
@@ -1,5 +1,6 @@
 import pygame
 from Menu import*
+from Sounddesign import*
 pygame.init()
 screen=pygame.display.set_mode((width,height))
 front=pygame.font.Font("font.ttf",150)
@@ -8,6 +9,7 @@ def afficher_gameover():
     tps_debut=pygame.time.get_ticks()
     while pygame.time.get_ticks()-tps_debut<=2000:
         pygame.event.get()
+        Sounddeath.play()
         text_surface=front.render("Game Over",True, (255,0,0))
         text_rect=text_surface.get_rect()
         text_rect.center=(width/2,height/2)
