2c890a668a3318fabeaae6cad36aeac793e7cfe3
Portono <evan.miniscloux@gmail.com>
2026-01-24 19:05:13 +0100

    Handle window close event in pause menu
    Modify pause menu to handle window close event.

--- menu_pause.py
+++ menu_pause.py
@@ -5,8 +5,8 @@ def afficher_menu_pause():
     en_pause = True
     while en_pause:
        for event in pygame.event.get():
-              if event.type == pygame.KEYDOWN:
-                     if event.key == pygame.K_ESCAPE:
-                            return  # Quitter le menu pause
+              if event.type == pygame.QUIT:
+                   pygame.quit()
+                   exit()
        if boucle_menu(pause=True):
              return  # Quitter le menu pause
