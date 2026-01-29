bf0d4afd622251a8a3aced822224b1618637dd5a
Enma-EB <berthelinevan@gmail.com>
2026-01-29 13:32:42 +0100

    Update gameover.py

--- In-game/gameover.py
+++ In-game/gameover.py
@@ -3,7 +3,7 @@ from Menu import*
 from Sounddesign import*
 pygame.init()
 screen=pygame.display.set_mode((width,height))
-front=pygame.font.Font("font.ttf",150)
+front=pygame.font.Font("fontgameover.ttf",int(width/7))
 
 def afficher_gameover():
     tps_debut=pygame.time.get_ticks()
@@ -13,5 +13,6 @@ def afficher_gameover():
         text_surface=front.render("Game Over",True, (255,0,0))
         text_rect=text_surface.get_rect()
         text_rect.center=(width/2,height/2)
+        screen.fill((0,0,0))
         screen.blit(text_surface,text_rect)
         pygame.display.flip()
