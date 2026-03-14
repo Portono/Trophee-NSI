ea3c0d1cd70cb5b0d5d1bf127a13c2a7bf7be853
Portono <evan.miniscloux@gmail.com>
2026-03-14 17:36:50 +0100

    importation des sons

--- sources/Sounddesign.py
+++ sources/Sounddesign.py
@@ -2,6 +2,6 @@ import pygame
 
 pygame.mixer.init()
 
-Soundhit=pygame.mixer.Sound("Hit.mp3")
-Sounddeath=pygame.mixer.Sound("Death.mp3")
+Soundhit=pygame.mixer.Sound("data/Hit.mp3")
+Sounddeath=pygame.mixer.Sound("data/Death.mp3")
 Sounddeath.set_volume(0.35)
